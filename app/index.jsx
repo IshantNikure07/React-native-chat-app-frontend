bcef448ddd599ead7ef7357056356d962cbafc2d
Ishant <ishantnikure712@gmail.com>
2026-03-19 19:56:08 +0530

    done with tailwind setup etc

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SplashScreen = () => {
+  return (
+    <View>
+      <Text className='text-3xl font-bold text-red-500'>SplashScreen</Text>
+    </View>
+  )
+}
+
+export default SplashScreen
+
+const styles = StyleSheet.create({})
